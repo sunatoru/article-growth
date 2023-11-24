b1f6c12d59b1c5d1e82e4d5a75e8df006f972b80
tano <supright34@gmail.com>
2023-11-25 00:09:18 +0900

    rubocopを導入後コードを修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name                  {Faker::Name.initials(number: 4)}
-    email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    name                  { Faker::Name.initials(number: 4) }
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
   end
 end
