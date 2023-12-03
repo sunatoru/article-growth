205ce6a52bfeefee20f8da4c35b2ba96f483ff9d
tano <supright34@gmail.com>
2023-12-03 12:49:49 +0900

    いいね機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
   resources :articles do
+    resource :likes, only: [:create, :destroy]
     collection do
       get 'search'
     end
